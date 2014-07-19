4b88430c55ce9b08de41fc7cae4925139026adb8
Carlos Salvatierra <cslucano@gmail.com>
2014-07-19 03:14:30 -0500

    first commit working

---
+++ jne/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for jne project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'jne'
+
+SPIDER_MODULES = ['jne.spiders']
+NEWSPIDER_MODULE = 'jne.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'jne (+http://www.yourdomain.com)'
---
+++ jne/spiders/candidatos_spider.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import time
+import urlparse
+import re
+from urlparse import urlsplit
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from scrapy.http import Request
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+from jne.items import CandidatoItem
+
+class CandidatosSpider(Spider):
+    name = 'candidatos'
+    allowed_domains = ['200.48.102.67']
+    start_urls = [ 'http://200.48.102.67/pecaoe/sipe/HojaVida.htm?' ]
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
+    def parse(self, response):
+        base_url = response.url
+        paginas = range(1,100)
+        for pagina in paginas:
+           url = '%sc=%s&p=72&op=140' % (base_url, pagina)
+           yield Request(url, callback=self.parse_page)
+
+    def parse_page(self, response):
+        self.driver.get(response.url)
+        
+        time.sleep(5)
+
+        candidatoItem = CandidatoItem()
+
+        cargo_postula = self.driver.find_element_by_xpath('//span[@id="txtCargoPostula"]').text
+
+        candidatoItem['cargo_postula'] = cargo_postula
+
+        return candidatoItem
+
+
